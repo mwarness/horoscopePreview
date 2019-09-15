81d22dbc6b508cfba3905173bd8c82691bf23d3b
Mark Warness <markwarness@gmail.com>
2019-09-15 13:16:49 -0400

    slight tweak

--- js/horodopes.js
+++ js/horodopes.js
@@ -1,14 +1,57 @@
-function Capicorn() {
-    alert('Capicorns ROCK')
-  }
+// js to call on horoscope api and append data to links on horoscope page
 
-function Cancer() {
-    alert('Cancers are BETTER')
+
+function guesthoro() {
+    var sign = $(".horoclass").data("value");  
+
+        $.ajax({
+
+            type: "GET",
+            url: "http://sandipbgt.com/theastrologer/api/horoscope/" + sign + "/today/"
+                // data: console.log(JSON.stringify(data))
+        }).then(function(data) {
+            var parseData = JSON.parse(data)
+            console.log(parseData);
+            $("#horodopeLinks").hide()
+            var horoscopeDisplay = $("#today")
+            var newDiv = $("<div>")
+            var newText = $("<p>")
+            var newDate = $("<p>")
+
+            newText.text(parseData.horoscope)
+            newText.addClass("today-horoscope")
+            newDate.text(parseData.date)
+            newDiv.append(newText)
+          
+            horoscopeDisplay.append(newDiv)
+
+
+        })
+ 
 }
 
-function Aquarius() {
-    alert('WE are better then you all')
-}
+
+
+
+
+
+
+
+
+
+
+
+// function Capicorn() {
+//     alert('Capicorns ROCK')
+//   }
+
+// function Cancer() {
+//     alert('Cancers are BETTER')
+// }
+
+// function Aquarius() {
+//     alert('WE are better then you all')
+// }
 
 
 // Create function that creates a var that holds the value of the sign (i.e Cancer, Leo, etc.)
